50560fbb0fe78e3e1a261de99b05b279c1b61357
Raipis <Romka995@gmail.com>
2017-11-22 01:32:28 +0100

    Under Construction
    Beep Beep

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>In construction</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container-fluid text white">
+    <div class="jumbotron bg-info text-center">
+      <h1 class="display-1">Under Construction</h1>
+      <h2 class="text-muted">Still working on the research</h2>
+      <p>much more cool stuff coming soon</p>
+    </div>
+  </div>
+  </body>
+</html>
